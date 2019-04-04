4fe014d7f52ba61c86d203d0cd8276b15a26064a
Maksim <maks112v@gmail.com>
2019-04-04 11:34:30 -0400

    basic front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
+import Login from "./Auth/Login";
+import Register from "./Auth/Register";
+import Users from "./Users";
 
 class App extends Component {
   render() {
@@ -8,9 +11,18 @@ class App extends Component {
         <div>
           <h1>Welcome</h1>
         </div>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/users">Users</Link>
+
         <Switch>
-          <Route />
-          <Route />
+          <Route exact path="/login" render={props => <Login {...props} />} />
+          <Route
+            exact
+            path="/register"
+            render={props => <Register {...props} />}
+          />{" "}
+          <Route exact path="/users" render={props => <Users {...props} />} />
         </Switch>
       </>
     );
